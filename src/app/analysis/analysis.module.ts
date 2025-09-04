2debd5edfc07a4a8032287f4b59418bcb6708962
unknown <eshikarupani2519@gmail.com>
2025-09-04 20:57:59 +0530

    created home and schemes page

--- src/app/analysis/analysis.module.ts
+++ src/app/analysis/analysis.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { CropInfoFormComponent } from './crop-info-form/crop-info-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AnalysisPageComponent } from './analysis-page/analysis-page.component';
+import { HomeComponent } from './home/home.component';
+import { SchemesComponent } from './schemes/schemes.component';
 
 
 
 @NgModule({
   declarations: [
     CropInfoFormComponent,
-    AnalysisPageComponent
+    AnalysisPageComponent,
+    HomeComponent,
+    SchemesComponent
   ],
   imports: [
     CommonModule,
